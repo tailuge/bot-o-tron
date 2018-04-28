0af137d7acc8fe4f87b1d718897e2d5ccda94cd2
tailuge <tailuge@gmail.com>
2018-04-28 12:45:31 +0000

    web based version

--- src/LichessApi.js
+++ src/LichessApi.js
@@ -54,38 +54,44 @@ class LichessApi {
   }
 
   get(URL) {
+    console.log(`GET ${URL}`);
+    console.log(JSON.stringify(this.headers));
     return axios.get(URL, {
-      baseURL: this.baseURL,
-      headers: this.headers
-    }).catch(err => console.log(err.response.data));
+        baseURL: this.baseURL,
+        headers: this.headers
+      }).then(data => { console.log(JSON.stringify(data.data)); return data; })
+      .catch(err => console.log(err));
   }
 
   post(URL, body) {
+    console.log(`POST ${URL} `+JSON.stringify(body || {}));
     return axios.post(URL, body || {}, {
-      baseURL: this.baseURL,
-      headers: this.headers
-    }).catch(err => console.log(err.response.data));
+        baseURL: this.baseURL,
+        headers: this.headers
+      }).then(data => { console.log(JSON.stringify(data.data)); return data; })
+      .catch(err => console.log(err.response.data));
   }
 
   /**
    * Connect to stream with handler.
    */
-  stream(url, handler) {
+  stream(URL, handler) {
+    console.log(`GET ${URL}`);
     axios({
         method: "get",
-        url: this.baseURL + url,
+        url: this.baseURL + URL,
         headers: this.headers,
         responseType: "stream"
       })
       .then(stream => stream.data.on("data", data => {
         data = data.toString("ascii").trim();
         if (data) {
+          console.log(`STREAM data : ${data}`);
           handler(JSON.parse(data));
         }
       }))
       .catch(err => console.log(err));
   }
-
 }
 
 module.exports = LichessApi;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/RobotUser.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'library.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'library'
   }
 };
