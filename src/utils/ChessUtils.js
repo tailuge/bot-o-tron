d38b91e70be8b3652df39f472d0de0181c9166b5
tailuge <tailuge@gmail.com>
2023-07-20 09:20:35 +0000

    deps

--- src/utils/ChessUtils.js
+++ src/utils/ChessUtils.js
@@ -8,6 +8,17 @@ class ChessUtils {
     fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
   ) {
     this.chess = new Chess(fen)
+    // prettier-ignore
+    this.SQUARES = [
+          'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+          'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+          'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+          'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+          'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+          'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+          'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+          'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
+    ]
   }
 
   reset() {
@@ -49,7 +60,7 @@ class ChessUtils {
   }
 
   squaresOf(colour) {
-    return this.chess.SQUARES.filter((square) => {
+    return this.SQUARES.filter((square) => {
       const r = this.chess.get(square)
       return r && r.color === colour
     })
@@ -106,11 +117,11 @@ class ChessUtils {
   }
 
   inCheckmate() {
-    return this.chess.in_checkmate()
+    return this.chess.isCheckmate()
   }
 
   inStalemate() {
-    return this.chess.in_stalemate()
+    return this.chess.isStalemate()
   }
 
   materialEval() {
